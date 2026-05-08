db24fac331fe3ae4504a75dffb8f4d48e4d75fca
juanserdiuk <juanstepanenko@hotmail.com>
2026-05-08 12:16:22 -0700

    fix: switch to webpack, externalize chromium and puppeteer-core

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@sparticuz/chromium', '@sparticuz/chromium-min', 'puppeteer-core'],
+
+  experimental: {
+    serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+  },
+
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), '@sparticuz/chromium', 'puppeteer-core']
+    return config
+  },
 };
 
 export default nextConfig;
